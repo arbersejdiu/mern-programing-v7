d722d0d29fdf83a8d1c1f3aae7ff55efc8a3ee63
arbersejdiu <arbersejdiu.upwork@gmail.com>
2024-03-25 16:30:18 +0100

    Create Sign in -route auth api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { errorHandeler } from "../../utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -12,6 +13,25 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) return res.status(401).json({ message: "User not found" });
+
+    const isMatch = bcryptjs.compareSync(password, validuser.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid password" });
+    const token = jwt.sign({ _id: validuser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validuser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
