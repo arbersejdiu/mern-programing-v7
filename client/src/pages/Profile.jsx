ba5aec6c1e3a4912b5325f8a5f1660a4cc09dd24
arbersejdiu <arbersejdiu.upwork@gmail.com>
2024-03-27 07:26:10 +0100

    Completed user Update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,11 +16,12 @@ import { useDispatch } from "react-redux";
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector(state => state.user);
+  const { currentUser, loading, error } = useSelector(state => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
 
   // Firebase
@@ -78,7 +79,6 @@ export default function Profile() {
         dispatch(updateUserFailure(data.message));
         return;
       }
-
       dispatch(updateUserSuccess(data));
       setUpdateSuccess(true);
     } catch (error) {
@@ -142,15 +142,20 @@ export default function Profile() {
             onChange={handleChange}
           />
           <button
+            disabled={loading}
             type="submit"
             className="bg-slate-500 p-2 rounded-md text-white hover:opacity-95">
-            Update
+            {loading ? "Loading" : "Update"}
           </button>
         </form>
         <div className="flex justify-between my-2">
           <span className="text-red-600 cursor-pointer">Delete Account</span>
           <span className="text-red-600 cursor-pointer">Sign Out</span>
         </div>
+        <p className="text-red-700 mt-5">{error ? error : ""}</p>
+        <p className="text-green-700 mt-5">
+          {updateSuccess ? "Updated successfully" : ""}
+        </p>
       </div>
     </>
   );
