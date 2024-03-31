e26c4d8b8169d206edd449f54d1cc172dcb250a9
arbersejdiu <arbersejdiu.upwork@gmail.com>
2024-03-31 14:14:50 +0200

    add search more button and functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -14,6 +14,7 @@ export default function Search() {
   });
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
   console.log(listings);
 
   useEffect(() => {
@@ -50,6 +51,9 @@ export default function Search() {
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if (data.length > 8) {
+        setShowMore(true);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -97,6 +101,19 @@ export default function Search() {
     urlParams.set("order", sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("startIndex", startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
   return (
     <>
@@ -218,6 +235,16 @@ export default function Search() {
               listings.map(listing => (
                 <ListingItem key={listing._id} listing={listing} />
               ))}
+
+            {showMore && (
+              <button
+                onClick={() => {
+                  onShowMoreClick();
+                }}
+                className="bg-[#4c2aa382]  p-2 text-white rounded-md my-5  cursor-pointer hover:opacity-95">
+                Show More
+              </button>
+            )}
           </div>
         </div>
       </div>
